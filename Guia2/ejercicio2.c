2010273e512594d71437fd3fde65e1927218157d
GRUPO2-Stuardo-Norero <48808036+GRUPO2-Stuardo-Norero@users.noreply.github.com>
2019-04-04 15:37:44 -0300

    Update 1
    Programa completo terminado

--- Guia2/ejercicio2.c
+++ Guia2/ejercicio2.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-int main(){
+float celcius(int s)
+{
+    float FA = (s/4)+40;
+    float C = (FA-32)*5/9;
+    return C;
+}
 
-	int FA;
-	int S;
-
-	FA = S/4+40;
-	return 0;
-
-
+int main()
+{
+    int S;
+    float FA;
+    
+    printf("ingrese el numero de veces que sonó al chicharra\n");
+    scanf("%d",&S);
+    
+    FA = (S/4)+40;
+    float C = celcius(S);
+    printf("La temperatura en fahrenheit es %f\n", FA);
+    printf("La temperatura en celcius es %f\n", C);
+    return 0;
 }
