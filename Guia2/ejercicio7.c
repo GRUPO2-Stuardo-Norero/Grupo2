e0976028a33670bc4db71f1f0d81a0464ec3786c
GRUPO2-Stuardo-Norero <48808036+GRUPO2-Stuardo-Norero@users.noreply.github.com>
2019-04-11 20:26:23 -0400

    Actualización
    Ahora si funciona

--- Guia2/ejercicio7.c
+++ Guia2/ejercicio7.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char cadena[50];
-    char salida[50];
-    printf("Ingrese la cadena de numeros y letras\n");
-    getc(cadena);
-    for(i=0;cadena[i]!='\0';i++){
-        val = atoi(cadena[i]);
-        i++;
-        character = cadena[i];
-        for(j=0;j<val;j++){
-            printf("%c",character);
+    char cad[50];
+    int cad2[50]; int cad3[50]; int j = 0; int i = 0; int largo = 0;
+       
+    printf("ingrese palabra alfanumerica\n");
+    scanf("%s",cad);
+    for(i = 0; i<sizeof(cad); i=i+2){
+        cad2[i] = cad[i]; 
+        cad3[i] = cad2[i]-48; 
+        while(j < cad3[i]){  
+            printf("%c", cad[i+1]);
+            j++;
         }
+        j=0;   
     }
-        
 
+    printf("\n");
     return 0;
 }
